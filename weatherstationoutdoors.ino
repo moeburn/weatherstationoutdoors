f27e7573c40d4f95007f9ce0d70e82520b2e81c4
moeburn <moeburn@gmail.com>
2023-07-07 22:35:30 -0400

    added pm25 RGB LED

--- weatherstationoutdoors.ino
+++ weatherstationoutdoors.ino
@@ -73,7 +73,7 @@ int windcount = 0;
 unsigned long lastmillis = 0;
 unsigned long millisBlynk = 0;
 unsigned long deltamillis = 0;
-unsigned long pagemillis = 0;
+unsigned long rgbmillis = 0;
 int pagetimer = 0;
 
 int xPos = 0;
@@ -88,7 +88,29 @@ char deltatime[64];
 
 int timerPhant = 3;
 
+int menuValue, zebraR, zebraG, zebraB, sliderValue;
+float  pmR, pmG, pmB;
+bool rgbON = true;
+
 PMS7003Serial<USARTSerial> pms7003(Serial1, D6);  //start up the PMS laser particle counter
+
+WidgetTerminal terminal(V19); //terminal widget
+
+BLYNK_WRITE(V21)
+{
+   menuValue = param.asInt(); // assigning incoming value from pin V1 to a variable
+}
+
+BLYNK_WRITE(V18)
+{
+     zebraR = param[0].asInt();
+     zebraG = param[1].asInt();
+     zebraB = param[2].asInt();
+}
+
+BLYNK_WRITE(V19)
+{terminal.flush();}
+
 
 void setup() { //This is where all Arduinos store the on-bootup code
   //RGB.control(true); //Turn off Photon's pulsing blue LED
@@ -101,18 +123,61 @@ void setup() { //This is where all Arduinos store the on-bootup code
     bme.setGasHeater(320, 150); // 320*C for 150 ms
     delay(5000); //Required to stabilize wifi
     Blynk.begin(auth, IPAddress(192,168,50,197), 8080);
+    terminal.clear();
+    terminal.println("********************************");
+    terminal.println("STARTING OUTDOOR WEATHER STATION");
+    terminal.println(Time.timeStr()); //print current time to Blynk terminal
+    terminal.println("********************************");
+    terminal.flush();
     Time.zone(-5);
     sensors.begin();
-    pagemillis = millis();
 }
 
 void loop() { //This is where all Arduinos store their "do this all the time" code
-  pms7003.Read();
-  Blynk.run();
-  if (millis() - millisBlynk >= 30000) //every 30 seconds
-  {
-    millisBlynk = millis();
-        timerBlynk = TTU;
+    pms7003.Read();
+    Blynk.run();
+    if (menuValue == 1) {RGB.control(false);}
+    if (menuValue == 2) 
+    {
+        pmG = 55 - sliderValue;
+        if (pmG < 0) {pmG = 0;}
+        pmG *= (255.0/55.0);
+        if (pmG > 255) {pmG = 255;}
+        
+        pmR = sliderValue;
+        if (pmR < 0) {pmR = 0;}
+        pmR *= (255.0/55.0);
+        if (pmR > 255) {pmR = 255;}
+        
+        pmB = sliderValue - 100;
+        if (pmB < 0) {pmB = 0;}
+        pmB *= (255.0/55.0);
+        if (pmB > 255) {pmB = 255;}
+        
+        RGB.control(true);
+        
+        if (rgbON == true) {RGB.color(pmR, pmG, pmB);}
+        else {RGB.color(0, 0, 0);}
+        if (sliderValue > 55)
+            {
+                if (millis() - rgbmillis >= 500)
+                {
+                    if (rgbON) {rgbON = false;}
+                    else {rgbON = true;}
+                    rgbmillis = millis();
+                }
+            }
+        else rgbON = true;
+    } 
+    if (menuValue == 3) 
+        {
+        RGB.control(true);
+        RGB.color(zebraR, zebraG, zebraB);
+        }
+    
+    if ((millis() - millisBlynk >= 30000) || (firstvalue == 1)) //every 30 seconds OR on first boot
+    {
+        millisBlynk = millis();
         bme.performReading();
         sensors.requestTemperatures();
         tempBME = bme.temperature;
@@ -132,12 +197,12 @@ void loop() { //This is where all Arduinos store their "do this all the time" co
             if (new2p5 - old2p5 > 50) {new2p5 = old2p5;}
             if (new10 - old10 > 50) {new10 = old10;}
         }
-
-
+        
+        
         abshumBME = (6.112 * pow(2.71828, ((17.67 * tempBME)/(tempBME + 243.5))) * humBME * 2.1674)/(273.15 + tempBME); //calculate absolute humidity
         dewpoint = tempBME - ((100 - humBME)/5); //calculate dewpoint
         humidex = ds18temp + 0.5555 * (6.11 * pow(2.71828, 5417.7530*( (1/273.16) - (1/(273.15 + dewpoint)) ) ) - 10); //calculate humidex using Environment Canada formula
-        
+        sliderValue = new2p5;
         Blynk.virtualWrite(V0, ds18temp);
         Blynk.virtualWrite(V1, (bme.pressure / 100.0));
         Blynk.virtualWrite(V2, dewpoint);
@@ -147,22 +212,24 @@ void loop() { //This is where all Arduinos store their "do this all the time" co
         Blynk.virtualWrite(V6, bme.temperature);
         Blynk.virtualWrite(V7, gasBME);
         Blynk.virtualWrite(V8, new1p0);
-		Blynk.virtualWrite(V9, new2p5);
-		Blynk.virtualWrite(V10, new10);
-		Blynk.virtualWrite(V11, pms7003.GetData(pms7003.count0_3um));
-		Blynk.virtualWrite(V12, pms7003.GetData(pms7003.count0_5um));
-		Blynk.virtualWrite(V13, pms7003.GetData(pms7003.count1um));
-		Blynk.virtualWrite(V14, pms7003.GetData(pms7003.count2_5um));
-		Blynk.virtualWrite(V15, pms7003.GetData(pms7003.count5um));
-		Blynk.virtualWrite(V16, pms7003.GetData(pms7003.count10um));
-		Blynk.virtualWrite(V17, humidex);
-		
-		old1p0 = new1p0; //reset data spike check variable
-		old2p5 = new2p5;
+        Blynk.virtualWrite(V9, new2p5);
+        Blynk.virtualWrite(V10, new10);
+        Blynk.virtualWrite(V11, pms7003.GetData(pms7003.count0_3um));
+        Blynk.virtualWrite(V12, pms7003.GetData(pms7003.count0_5um));
+        Blynk.virtualWrite(V13, pms7003.GetData(pms7003.count1um));
+        Blynk.virtualWrite(V14, pms7003.GetData(pms7003.count2_5um));
+        Blynk.virtualWrite(V15, pms7003.GetData(pms7003.count5um));
+        Blynk.virtualWrite(V16, pms7003.GetData(pms7003.count10um));
+        Blynk.virtualWrite(V17, humidex);
+        terminal.print("Last update: ");
+        terminal.print(Time.timeStr()); //print current time to Blynk terminal
+        terminal.println("");
+        terminal.flush();
+        old1p0 = new1p0; //reset data spike check variable
+        old2p5 = new2p5;
         old10 = new10;
         firstvalue = 0; //we have run once now, no longer first run
-      
-  }
+    }
 }
 
 
